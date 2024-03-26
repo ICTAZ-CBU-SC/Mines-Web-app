9dee26edd0e830a243f607851b3d5f181e27bdd8
joshua-inf <jsibanda407@gmail.com>
2024-03-26 22:20:29 +0200

    .

--- src/components/Headers_and_footers/Nav.js
+++ src/components/Headers_and_footers/Nav.js
@@ -3,27 +3,35 @@ import { HiMenuAlt2 } from "react-icons/hi";
 
 const NavBar = () => {
     const [slider, setSlider] = useState(false)
-    let slidenav = document.getElementById('slidenav')
+     
 
+
+    
 
     const ActivateSlider = () => {
-        setSlider(!slider)
-
-        document.getElementById('slidenav').style.display = "none";
         if(slider){
+            document.getElementById('slidenav').classList.remove('slidenav_2')
+            document.getElementById('slidenav').classList.add('slidenav_1')
+        } else {
+            document.getElementById('slidenav').classList.remove('slidenav_1')
+            document.getElementById('slidenav').classList.add('slidenav_2')
         }
+
+        setSlider(!slider)
     }
 
     
     return (
         <>
             <div>
-                <div style={{position:'relative'}}>
-                    <div className="slidenav" style={{height:'100vh', width:'150px'}}>
+                <div className="bg-light" style={{position:'relative'}}>
+                    <div className="" id="slidenav" style={{height:'100vh'}}>
+                        <div style={{}}>
 
+                        </div>
                     </div>
-                    <div style={{position:'absolute', top:'10px', right:'10px'}}>
-                        <HiMenuAlt2 onClick={() => ActivateSlider()} size={40} />
+                    <div style={{position:'fixed', top:'10px', left:'10px'}}>
+                        <HiMenuAlt2 style={{cursor:'pointer'}} onClick={() => ActivateSlider()} size={40} />
                     </div>
                 </div>
             </div>
