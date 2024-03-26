99b35773482d0078c7a5dd08a62550d083c2a3b7
edgar <edgarmwila84@gmail.com>
2024-03-26 22:48:11 +0200

    added mapping algorithm

---
+++ src/components/mapping.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Mapping = () => {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+
+  useEffect(() => {
+    // Function to simulate movement
+    const moveDot = () => {
+      // Example: Update latitude and longitude every 30 seconds
+      setInterval(() => {
+        // Update latitude and longitude randomly (for demonstration purposes)
+        setLatitude(10 * 180 - 90);{/*replace with variables from firebase location */}
+        setLongitude(10 * 360 - 180);{/*replace with variables from firebase location */}
+      }, 30000);
+    };
+
+    moveDot(); // Start moving the dot
+
+    // Cleanup function to stop interval on unmount
+    return () => clearInterval(moveDot);
+  }, []); // Empty dependency array to ensure effect runs only once
+
+  return (
+    <div className="container">
+      <div className="map-container">
+        <div
+          className="dot"
+          style={{
+            position: 'absolute',
+            top: `${50 + latitude}%`, // Example: Offset by 50%
+            left: `${50 + longitude}%`, // Example: Offset by 50%
+            backgroundColor: 'red',
+            width: '10px',
+            height: '10px',
+            borderRadius: '50%',
+          }}
+        ></div>
+        <img
+          src="./images/map.jpeg"
+          alt="Map"
+          style={{ maxWidth: '100%', height: 'auto' }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Mapping;
