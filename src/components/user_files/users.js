b3ef09ac442fd08874515268c4b4ae2de7487e2e
Palanga123 <palangageorgen@gmail.com>
2024-03-28 14:00:42 +0200

    hi

--- src/components/user_files/users.js
+++ src/components/user_files/users.js
@@ -1,4 +1,4 @@
-import image from "../images/map.jpg"
+import image from "../images/logo512.png"
 const user = {
     name: "Joshua Sibanda",
     position: 'Worker',
@@ -10,9 +10,9 @@ export default function Profile(){
     return (
         <>
             <main style={{margin:'50px',}}> 
-                <div style={{paddingBottom:'10px',paddingTop:'10px',width:'270px', paddingInline:'5px', background:'#64748b', borderRadius:'10px'}}>
+                <div style={{paddingBottom:'10px',paddingTop:'10px',width:'270px', paddingInline:'5px', background:'#e2e8f0', borderRadius:'10px'}}>
                     <div style={{display:'flex', justifyContent:'flex-end', verticalAlign:'middle',marginRight:'5px'}}>
-                        <p style={{display:'block', padding:'1px 3px', marginRight:'5px', borderRadius:'5px', background:'#e2e8f0', fontWeight:'bold', fontSize:'10px'}}>Gas | 30%</p>
+                        <p style={{display:'block', padding:'1px 3px', marginRight:'5px', borderRadius:'5px', background:'#f8fafc', fontWeight:'bold', fontSize:'10px'}}>Gas | 30%</p>
                         <div className='' style={{height:'10px', width: '10px', background:'red', borderRadius: '50%' }} ></div>
                     </div>
                     <div style={{display:'flex', justifyContent:'flex-end', alignItems:'center', marginRight:'3px'}}>
