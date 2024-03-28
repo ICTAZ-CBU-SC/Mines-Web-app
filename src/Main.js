6d1ba5bf66b27a7673b185120fba634e083b0f43
edgar <edgarmwila84@gmail.com>
2024-03-28 22:01:32 +0200

    Added documentation

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,18 @@
-import GasCheck from "./components/GasCheck";
-import MapComp from "./components/MapComp";
+
+import GasCheck from './components/GasCheck';
+import MapComp from './components/VideoFeed';
+import { useLocation } from "react-router-dom";
 
 const MainPage = () => {
-   
+    const location = useLocation();//get information from link via the useLocation method.
+
     return (
-        <>
-            <div className="ps-5 ms-3 p-4 d-flex flex-column" style={{minHeight:'100vh', gap:'20px'}}>
-                <div className="">
-                    <GasCheck/>
-                </div>
-                <MapComp/>
+        <div className="ps-5 ms-3 p-4 d-flex flex-column" style={{minHeight:'100vh', gap:'20px'}}>
+            <div>
+                <GasCheck {...location.state}/>{/*parse data through location.state to the component*/}
             </div>
-        </>
-    )
+            <MapComp {...location.state}/>
+        </div>
+    );
 }
-
-
 export default MainPage;
