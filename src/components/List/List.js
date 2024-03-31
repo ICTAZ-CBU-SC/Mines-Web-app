5bbb394e25af7e722a8a507d3f02d7b797f9456d
Palanga123 <palangageorgen@gmail.com>
2024-03-31 16:53:48 +0200

    Our team Page

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,45 +1,95 @@
 import Profile from './AppData.js';
 import { Link } from "react-router-dom";
+import amos from '../images/amos.jpeg';
+import bulaya from '../images/bulaya.jpeg';
+import george from '../images/george.jpeg';
+import gloria from '../images/gloria.jpeg';
+import zimba from '../images/zimba.jpeg';
+import dingiswayo from '../images/dingiswayo.jpeg';
+import benjamin from '../images/benjamin.jpeg';
+import clivet from '../images/clivet.jpeg';
+import harriet from '../images/harriet.jpeg';
+import edgar from '../images/edgar.jpeg';
+import temwani from '../images/temwani.jpeg';
+import gilbert from '../images/gilbert.jpeg';
+import songiso from '../images/songiso.jpeg';
+import joshua from '../images/joshua.jpeg';
+
 
 function List() {
 
 
     const info = [
+        
         {
-            name: 'Bulaya Mwanaute',
-        },
-        {
-            name: 'Gloria Chomba',
+            name: 'Benjamin Ngulube',
+            image: benjamin,
+            position: 'President',
         },
         {
             name: 'Zimba Isaac',
+            image: zimba,
+            position: "Vice President"
         },
         {
             name: 'Clivet Lungu',
+            image: clivet,
+            position: "Projects Coordinator"
+        },
+        {
+            name: 'Gilbert N\'gandu',
+            image: gilbert,
+            position: "Developer",
+        },
+        {
+            name: 'Bulaya Mwanaute',
+            image: bulaya,
+            position: "Developer",
+        },
+        {
+            name: 'Gloria Chomba',
+            image: gloria,
+            position: "Developer",
         },
         {
             name: 'Amos Kanyanta',
+            image: amos,
+            position: "Developer",
         },
         {
-            name: 'Gilbert',
+            name: 'Songiso Moono',
+            image: songiso,
+            position: "Developer",
         },
         {
-            name: 'Zimba Isaac',
-        },
-        {
-            name: 'Clivet Bigman',
+            name: 'Edgar Mwila',
+            image: edgar,
+            position: "Developer",
         },
         {
             name: 'Joshua Sibanda',
+            image: joshua,
+            position: "Developer",
         },
         {
-            name: 'George Palanga',
+            name: 'Palanga Nkobi',
+            image: george,
+            position: "Developer",
         },
         {
-            name: 'Zimba Isaac',
+            name: 'Temwani',
+            image: temwani,
+            position: "Developer",
         },
         {
-            name: 'Clivet Bigman',
+            name: 'Dingiswayo Nkwashi',
+            image: dingiswayo,
+            position: "Developer",
+        },
+        {
+            name: 'Harriet Mulonda',
+            image: harriet,
+            position: "Developer",
         },
         
     ];
@@ -47,24 +97,27 @@ function List() {
     return(
         <>
 
-            <div className = "mt-8 mb-12 w-11/12 md:w-2/5 rounded-full p-4 m-auto text-center text-gray-700">
-                <p className="text-3xl font-semibold">
-                    Meet The<span className="font-bold text-sky-600 "> Developer Team</span>
-                </p>
-                <p className="py-2 text-gray-800 text-[16] underline underline-offset-8">Our team of brave sudents</p>
+            <div className="bg-gray-100">
+                <div className="mt-8 mb-12 w-11/12 md:w-2/5 rounded-full p-4 m-auto text-center text-gray-700">
+                    <p className="text-3xl font-semibold">
+                        Meet The<span className="font-bold text-sky-600 "> Developer Team</span>
+                    </p>
+                    <p className="py-2 text-gray-800 text-[16] underline underline-offset-8">Our team of brave sudents</p>
+                </div>
+
+                <main className="w-full rounded-3xl m-auto p-10 b rounded-2xl">
+                    <div className="w-4/5 m-auto">
+                        <div className="m-auto sm:grid-cols-2 grid md:grid-cols-4 gap-4">
+                            {info.map((e) => (<Link to={'/AppData' + e.id}>
+                                <Profile position = {e.position} image={e.image} name={e.name}></Profile>
+                            </Link>
+                            ))
+                            }
+                        </div>
+                    </div>
+                </main>
             </div>
 
-            <main className="w-full rounded-3xl m-auto p-10 b rounded-2xl">
-                <div className="w-4/5 m-auto">
-                    <div className="m-auto sm:grid-cols-2 grid md:grid-cols-4 gap-4">
-                        {info.map((e) => (<Link to={'/AppData' + e.id}>
-                            <Profile name={e.name}></Profile>
-                        </Link>
-                        ))
-                        }
-                    </div>
-                </div>
-            </main>
         </>
     )
 }
