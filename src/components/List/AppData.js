44cfb7e0d2313bb1374bdf8adf37df69d1809d91
Palanga123 <palangageorgen@gmail.com>
2024-03-31 17:53:32 +0200

    commit

--- src/components/List/AppData.js
+++ src/components/List/AppData.js
@@ -4,12 +4,14 @@ function Profile(props) {
     return(
         <>
 
-            <div className="text-center w-40 rounded-md py-4 m-auto my-2">
-                <img
-                    src={image}
-                    alt='image'
-                    className="m-auto h-36 w-36 rounded-full shadow-2xl border-[3px] border-green-600 p-1"
-                />
+            <div className="text-center bg-white rounded-md pb-4 m-auto my-2 shadow-md w-48">
+                <div className="overflow-hidden h-40 rounded-md">
+                    <img
+                        src={props.image}
+                        alt={props.name}
+                        className="w-full hover:scale-110 hover:duration-500"
+                    />
+                </div>
                 <p className="block pt-6 text-lg font-semibold text-gray-800">{props.name}</p>
                 <p className="block text-[14px] text-sky-600 font-semibold">{props.position}</p>
             </div>
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,4 +1,19 @@
 import Profile from './AppData.js';
+import { Link } from "react-router-dom";
+import amos from '../images/amos.jpeg';
+import bulaya from '../images/bulaya.jpeg';
+import george from '../images/george.jpeg';
+import gloria from '../images/gloria.jpeg';
+import zimba from '../images/zimba.jpeg';
+import dingiswayo from '../images/dingiswayo.jpeg';
+import benjamin from '../images/benjamin.jpeg';
+import clivet from '../images/clivet.jpeg';
+import harriet from '../images/harriet.jpeg';
+import edgar from '../images/edgar.jpeg';
+import temwani from '../images/temwani.jpeg';
+import gilbert from '../images/gilbert.jpeg';
+import songiso from '../images/songiso.jpeg';
+import joshua from '../images/joshua.jpeg';
 import { motion } from 'framer-motion'
 
 function List() {
@@ -72,37 +87,47 @@ function List() {
             position: "Developer",
         },
         {
-            name: 'Clivet Bigman',
+            name: 'Harriet Mulonda',
+            image: harriet,
+            position: "Developer",
         },
 
     ];
 
     return (
         <>
-            <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.5 }}
-            >
-                <div className="mt-8 mb-12 w-11/12 md:w-2/5 rounded-full p-4 m-auto text-center text-gray-700">
-                    <p className="text-3xl font-semibold">
-                        Meet The<span className="font-bold text-sky-600 "> Developer Team</span>
-                    </p>
-                    <p className="py-2 text-gray-800 text-[16] underline underline-offset-8">Our team of brave sudents</p>
-                </div>
 
-                <main className="w-full rounded-3xl m-auto p-10 b rounded-2xl">
-                    <div className="w-4/5 m-auto">
-                        <div className="m-auto sm:grid-cols-2 grid md:grid-cols-4 gap-4">
-                            {info.map((e) => (<div>
-                                <Profile name={e.name}></Profile>
-                            </div>
-                            ))
-                            }
+            
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+                >
+                    <div className="bg-gray-100">
+                    <div className="mt-8 mb-12 w-11/12 md:w-2/5 rounded-full p-4 m-auto text-center text-gray-700 flex justify">
+                        <div>
+                            <p className="text-3xl font-semibold">
+                                Meet The<span className="font-bold text-sky-600 "> Developer Team</span>
+                            </p>
+                            <p className="py-2 text-gray-800 text-[16] underline underline-offset-8">Our team of brave sudents</p>
                         </div>
                     </div>
-                </main>
-            </motion.div>
+
+                    <main className="w-full rounded-3xl m-auto p-10 b rounded-2xl">
+                        <div className="w-4/5 m-auto">
+                            <div className="m-auto sm:grid-cols-2 grid md:grid-cols-4 gap-4">
+                                {info.map((e) => (
+                                    <Link to={'/AppData' + e.id}>
+                                        <Profile position={e.position} image={e.image} name={e.name}></Profile>
+                                    </Link>
+                                ))}
+                            </div>
+                        </div>
+                    </main>
+                    </div>
+                </motion.div >
+            
+        
         </>
     )
 }
